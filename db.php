7ca53c19713f4d3ac262d76d6328c9460f759ef2
zii786 <168188404+zii786@users.noreply.github.com>
2025-04-06 20:06:16 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "telehealth";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
